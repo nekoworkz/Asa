4c9ba8b339ae56106c10455a08710edb0573d49e
nekoworkz <nekoworkz@gmail.com>
2016-08-11 00:08:07 -0600

    Update asa.h

--- asa.h
+++ asa.h
@@ -7,14 +7,15 @@ int drawFrame (
 	uintptr_t	polygonIndexP,	// Pointer to array of P vertice index, uint32_t elements
 	uintptr_t	polygonIndexQ,	// Pointer to array of Q vertice index, uint32_t elements
 	uintptr_t	polygonIndexR,	// Pointer to array of R vertice index, uint32_t elements
-	uintptr_t	polygonIndexT,	// Pointer to array of texture index, uintptr_t elements
+	uintptr_t	polygonIndexT,	// Pointer to array of texture index, uint32_t elements
 	uintptr_t	verticeArrayX,	// Pointer to array of vertices, float elements
 	uintptr_t	verticeArrayY,	// Pointer to array of vertices, float elements
 	uintptr_t	verticeArrayZ,	// Pointer to array of vertices, float elements
 	uintptr_t	verticeArrayU,	// Pointer to array of vertices, uint16_t elements
 	uintptr_t	verticeArrayV,	// Pointer to array of vertices, uint16_t elements
-	uintptr_t	UVindex,	// Pointer to texture map index
-	uintptr_t	textureIndex,	// Pointer to texture pointer index
+	uintptr_t	texturePointer,	// Pointer to array of texture pointers, uintptr_t elements
+	uintptr_t	textureWidth,	// Pointer to array of texture widths, uint16_t elements
+	uintptr_t	textureHeight,	// Pointer to array of texture heights, uint16_t elements
 	int32_t		cameraYaw,	// Camera Yaw angle in scaled units, 8LSB is ignored
 	int32_t		cameraRoll,	// Camera Roll angle in scaled units, 8LSB is ignored
 	int32_t		cameraPitch	// Camera Pitch angle in scaled units, 8LSB is ignored
