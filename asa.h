00b8ebcb68e17190aa1343b1da98a9a922cfa48c
nekoworkz <nekoworkz@gmail.com>
2016-08-11 05:29:02 -0600

    Update asa.h

--- asa.h
+++ asa.h
@@ -12,16 +12,39 @@ int drawFrame (
 	uintptr_t	objectRoll,	// Pointer to object roll array, elements in int32_t
 	uintptr_t	objectPitch,	// Pointer to object pitch array, elements in int32_t
 	uintptr_t	objectScale,	// Pointer to object scaling array, elements in float
+	uintptr_t	objectNumPoly,	// Pointer to object number of polygons array, elements in uint32_t
 	uintptr_t	objectPolygons,	// Pointer to object polygon array pointer array, elements in uintptr_t
+	uintptr_t	objectNumVert,	// Pointer to object number of vertices array, elements in uint32_t
 	uintptr_t	objectVertices,	// Pointer to object vertex array pointer array, elements in uintptr_t
 	uintptr_t	objectTexture,	// Pointer to object texture pointer array, elements in uintptr_t
 	int32_t		cameraYaw,	// Camera Yaw angle in scaled units, 8LSB is ignored
 	int32_t		cameraRoll,	// Camera Roll angle in scaled units, 8LSB is ignored
 	int32_t		cameraPitch	// Camera Pitch angle in scaled units, 8LSB is ignored
-	) {
+	)
+{
 		
-		// Cull Vertices not in scene
+		// Calculate maximum number of polygons and vertices
+		uint32_t maxNumPoly = sum ( numOfObjects, objectNumPoly );
+		uint32_t maxNumvert = sum ( numOfObjects, objectnumVert );
 		
-		cullInScene ();
-		
+}
+
+
+// Calculate sum of data set
+uint32_t sum (
+	numOfElements,	// Number of elements to sum,
+	elementArray	// Pointer to array of elements
+	)
+{
+	
+	// Declare accumulator
+	uint32_t E = 0;
+	
+	// Accumulate array elements
+	for (uint32_t i = 0; i!=numOfElements; i++;) {
+		E = E + elementArray[i];
 	}
+	
+	//Return value
+	return E;
+}
