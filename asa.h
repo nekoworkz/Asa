0ca632bb66269b206351002d42152c3e778b109d
nekoworkz <nekoworkz@gmail.com>
2016-08-14 07:31:27 -0600

    Update asa.h

--- asa.h
+++ asa.h
@@ -3,29 +3,48 @@
 
 // root graphics function. draws entire frame
 int drawFrame (
-	uintptr_t	frameBuffer,	// Pointer to framebuffer
-	uint16_t	numOfObjects	// Number of objects in scene
-	uintptr_t	objectX,	// Pointer to object X position array, elements in float
-	uintptr_t	objectY,	// Pointer to object Y position array, elements in float
-	uintptr_t	objectZ,	// Pointer to object Z position array, elements in float
-	uintptr_t	objectYaw,	// Pointer to object yaw array, elements in int32_t
-	uintptr_t	objectRoll,	// Pointer to object roll array, elements in int32_t
-	uintptr_t	objectPitch,	// Pointer to object pitch array, elements in int32_t
-	uintptr_t	objectScale,	// Pointer to object scaling array, elements in float
-	uintptr_t	objectNumPoly,	// Pointer to object number of polygons array, elements in uint32_t
-	uintptr_t	objectPolygons,	// Pointer to object polygon array pointer array, elements in uintptr_t
-	uintptr_t	objectNumVert,	// Pointer to object number of vertices array, elements in uint32_t
-	uintptr_t	objectVertices,	// Pointer to object vertex array pointer array, elements in uintptr_t
-	uintptr_t	objectTexture,	// Pointer to object texture pointer array, elements in uintptr_t
-	int32_t		cameraYaw,	// Camera Yaw angle in scaled units, 8LSB is ignored
-	int32_t		cameraRoll,	// Camera Roll angle in scaled units, 8LSB is ignored
-	int32_t		cameraPitch	// Camera Pitch angle in scaled units, 8LSB is ignored
+	uintptr_t	frameBuffer,		// Pointer to framebuffer
+	uint16_t	numOfObjects		// Number of objects in scene
+	uintptr_t	objectX,		// Pointer to object X position array, elements in float
+	uintptr_t	objectY,		// Pointer to object Y position array, elements in float
+	uintptr_t	objectZ,		// Pointer to object Z position array, elements in float
+	uintptr_t	objectYaw,		// Pointer to object yaw array, elements in int32_t
+	uintptr_t	objectRoll,		// Pointer to object roll array, elements in int32_t
+	uintptr_t	objectPitch,		// Pointer to object pitch array, elements in int32_t
+	uintptr_t	objectScale,		// Pointer to object scaling array, elements in float
+	uintptr_t	objectNumPoly,		// Pointer to object number of polygons array, elements in uint32_t
+	uintptr_t	objectPolygonP,		// Pointer to object P polygon array pointer array, elements in uintptr_t
+	uintptr_t	objectPolygonQ,		// Pointer to object Q polygon array pointer array, elements in uintptr_t
+	uintptr_t	objectPolygonR,		// Pointer to object R polygon array pointer array, elements in uintptr_t
+	uintptr_t	objectNumVert,		// Pointer to object number of vertices array, elements in uint32_t
+	uintptr_t	objectVerticesX,	// Pointer to object X vertex array pointer array, elements in uintptr_t
+	uintptr_t	objectVerticesY,	// Pointer to object Y vertex array pointer array, elements in uintptr_t
+	uintptr_t	objectVerticesZ,	// Pointer to object Z vertex array pointer array, elements in uintptr_t
+	uintptr_t	objectTexture,		// Pointer to object texture pointer array, elements in uintptr_t
+	int32_t		cameraYaw,		// Camera Yaw angle in scaled units, 8LSB is ignored
+	int32_t		cameraRoll,		// Camera Roll angle in scaled units, 8LSB is ignored
+	int32_t		cameraPitch		// Camera Pitch angle in scaled units, 8LSB is ignored
 	)
 {
 		
 		// Calculate maximum number of polygons and vertices
-		uint32_t maxNumPoly = sum ( numOfObjects, objectNumPoly );
-		uint32_t maxNumvert = sum ( numOfObjects, objectnumVert );
+		uint32_t maxNumPoly = sum ( numOfObjects, objectNumPoly );	// Calculate potential polygons
+		uint32_t maxNumvert = sum ( numOfObjects, objectnumVert );	// Calculate potential vertices
+		
+		// Declare scene polygon arrays
+		uint32_t scenePolygonsP [ maxNumPoly ];
+		uint32_t scenePolygonsQ [ maxNumPoly ];
+		uint32_t scenePolygonsR [ maxNumPoly ];
+		
+		// Populate scene polygon afrrays
+		importObjectPolygons ( maxNumPoly, scenePolygonsP, numOfObjects, objectNumPoly, objectPolygonP, objectNumVert );	// Copy all P indices
+		importObjectPolygons ( maxNumPoly, scenePolygonsQ, numOfObjects, objectNumPoly, objectPolygonQ, objectNumVert );	// Copy all Q indices
+		importObjectPolygons ( maxNumPoly, scenePolygonsR, numOfObjects, objectNumPoly, objectPolygonR, objectNumVert );	// Copy all R indices
+		
+		// Declare scene vertice arrays
+		
+		// Translate Object Vertices
+		translate ();
 		
 }
 
